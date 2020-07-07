c455ecccbaaba305474a9127f010f053cf857212
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-07-07 09:34:23 +0530

    Pattern for valid last name.

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,10 +1,22 @@
 #!/bin/bash -x
-echo "Welcome to User Registration Form"
+#Use Case 1- As a User need to enter a valid First Name.
+echo "Welcome to User Registration"
 
 echo "Enter User First Name :"
 read firstName;
-namepattern="^[A-Z][a-z]{3}$"
+namepattern="^[A-Z][a-z]{3,}$"
 if [[ $firstName =~ $namepattern ]]
+then
+    echo "Valid"
+else
+    echo "Invalid"
+fi
+
+#UseCase2- As a User need to enter a valid last Name.
+echo "Enter User last Name : "
+read lastName;
+lastNamepattern="^[A-Z][a-z]{3,}$"
+if [[ $lastName =~ $lastNamepattern ]]
 then
     echo "Valid"
 else
